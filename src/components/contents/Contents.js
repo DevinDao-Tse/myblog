58c7725c9b99b1a59a6c2bbf010f1b13ed62907f
Devin Dao-Tse <devindaotse@gmail.com>
2021-08-19 16:58:13 -0400

    added again

---
+++ src/components/contents/Contents.js
@@ -0,0 +1,17 @@
+import { props } from 'bluebird'
+import React, { useEffect, Fragment } from 'react'
+import BlogContent from './BlogContent'
+
+const Content = (props) => {
+
+  return (
+    <Fragment>
+      <BlogContent />
+    </Fragment>
+  )
+}
+
+
+
+
+export default Content
