5effda1b12fc4655f902ba19ae7cc8ed06ad6953
Devin Dao-Tse <devindaotse@gmail.com>
2021-08-19 16:57:21 -0400

    need testing, added main section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import './styles/App.css'
 import SimpleCard from './components/UI/SimpleCard'
 import MainHeader from './components/MainHeader/MainHeader';
+import Content from './components/contents/Contents';
 
 function App() {
   return (
     <div className="App">
       <MainHeader />
       <main>
-        <SimpleCard />
+        <SimpleCard>
+          <Content />
+        </SimpleCard>
       </main>
-
     </div>
   );
 }
