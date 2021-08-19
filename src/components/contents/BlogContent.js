c836d6360150e9ab6c106db3c2d4b565ed23bfad
Devin Dao-Tse <devindaotse@gmail.com>
2021-08-19 16:57:53 -0400

    added, need testing

---
+++ src/components/contents/BlogContent.js
@@ -0,0 +1,13 @@
+import classes from './BlogContent.module.css'
+
+
+//read from DB or text file or hard code
+const BlogContent = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BlogContent
